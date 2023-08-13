419932f8f3ce595b81498dfd55b243c634d52624
unknown <63268168+ramok0@users.noreply.github.com>
2023-08-13 23:27:27 +0200

    fix : remove logs in upgun::ReflectedObject::GetOffset

--- impl/libupgun.cpp
+++ impl/libupgun.cpp
@@ -1,28 +1,22 @@
 #include "../include/libupgun.hpp"
 
-uint16 upgun::ReflectedObject::GetOffset(const std::wstring ClassName, const std::wstring PropertyName)
+uint16 upgun::ReflectedObject::GetOffset(const std::wstring PropertyName)
 {
 	UClass Class = this->get_class_private().Cast<UClass>();
 	uint16 result;
-	std::wcout << Class.get_full_name() << std::endl;
 	if (result = Class.get_offset(PropertyName)) {
 		return result;
 	}
-
-	for (UObject outer = this->get_outer_private(); outer; outer = outer.get_outer_private())
+	UClass super;
+	while (super = Class.get_super_struct())
 	{
-		std::wcout << outer.get_full_name() << std::endl;
-		if (result = outer.Cast<UClass>().get_offset(PropertyName))
-		{
-			return result;
+		if (super) {
+			if (result = super.get_offset(PropertyName)) {
+				return result;
+			}
 		}
 
-		upgun::UClass outerClass = outer.get_class_private().Cast<UClass>();
-		std::wcout << outerClass.get_full_name() << std::endl;
-		if (result = outerClass.get_offset(PropertyName))
-		{
-			return result;
-		}
+		Class = super;
 	}
 
 	return 0;
